1728e5a47d465fb958cf3c1f738dc4f411d43ba7
skullman667 <jebrafman@comcast.net>
2015-04-01 19:53:34 -0400

    Scrolling and Slideshow

---
+++ slideshow/files/js/utilities.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$('.flexslider').flexslider({
+		animation:'slide'
+	});
+
+	$('.flexslider2').flexslider({
+		animation:'slide',
+		controlNav:'thumbnails'
+	});
+
+
+});
